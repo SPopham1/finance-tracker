7fc182888c4ce2f20b68e221f82b4091d84bfb10
SPopham1 <pophamsamuel@gmail.com>
2026-02-07 13:09:08 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Create list to store multiple expenses
+List<Expense> myExpenses = new List<Expense>();
+
+public class Expense
+{
+    public string Description { get; set; }
+    public decimal Amount { get; set; }
+    public string Category { get; set; }
+}
+
